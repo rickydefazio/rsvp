3095cb9f09cfd017f251902dce16eca9800e94d9
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2020-02-23 20:34:54 -0500

    Correct typos

--- src/MainContent/GuestList/Guest.js
+++ src/MainContent/GuestList/Guest.js
@@ -3,31 +3,35 @@ import PropTypes from 'prop-types';
 
 import GuestName from './GuestName';
 
-const Guest = props =>
+const Guest = props => (
   <li>
     <GuestName
       isEditing={props.isEditing}
-      handleNameEdits={e => props.setName(e.target.value)}>
+      handleNameEdits={e => props.setName(e.target.value)}
+    >
       {props.name}
     </GuestName>
     <label>
       <input
-        type="checkbox"
+        type='checkbox'
         checked={props.isConfirmed}
-        onChange={props.handleConfirmation} /> Confirmed
+        onChange={props.handleConfirmation}
+      />{' '}
+      Confirmed
     </label>
-    <button onClick={props.handeToggleEditing}>
-      {props.isEditing ? "save" : "edit"}
+    <button onClick={props.handleToggleEditing}>
+      {props.isEditing ? 'save' : 'edit'}
     </button>
     <button onClick={props.handleRemove}>remove</button>
-  </li>;
+  </li>
+);
 
 Guest.propTypes = {
   name: PropTypes.string.isRequired,
   isConfirmed: PropTypes.bool.isRequired,
   isEditing: PropTypes.bool.isRequired,
   handleConfirmation: PropTypes.func.isRequired,
-  handeToggleEditing: PropTypes.func.isRequired,
+  handleToggleEditing: PropTypes.func.isRequired,
   setName: PropTypes.func.isRequired,
   handleRemove: PropTypes.func.isRequired
 };
