26757d1a3019aae91492d3b2a614d428a0f5ca1c
Ricky DeFazio <rickysdefazio@gmail.com>
2020-03-07 17:34:42 -0500

    gh-pages

--- src/MainContent/GuestList/index.js
+++ src/MainContent/GuestList/index.js
@@ -4,23 +4,25 @@ import PropTypes from 'prop-types';
 import Guest from './Guest';
 import PendingGuest from './PendingGuest';
 
-const GuestList = props =>
+const GuestList = props => (
   <ul>
     <PendingGuest name={props.pendingGuest} />
     {props.guests
       .filter(guest => !props.isFiltered || guest.isConfirmed)
-      .map((guest, index) =>
+      .map((guest, index) => (
         <Guest
           key={index}
           name={guest.name}
           isConfirmed={guest.isConfirmed}
           isEditing={guest.isEditing}
           handleConfirmation={() => props.toggleConfirmation(guest.id)}
-          handeToggleEditing={() => props.toggleEditing(guest.id)}
+          handleToggleEditing={() => props.toggleEditing(guest.id)}
           setName={text => props.setName(text, guest.id)}
-          handleRemove={() => props.removeGuest(guest.id)} />
-      )}
-  </ul>;
+          handleRemove={() => props.removeGuest(guest.id)}
+        />
+      ))}
+  </ul>
+);
 
 GuestList.propTypes = {
   guests: PropTypes.array.isRequired,
